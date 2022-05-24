896c59b00da76897963c0ed5478d88164bcd53d3
DerLux <lukas.butscher@gmx.de>
2022-05-24 16:04:52 +0200

    it's alive

--- Aufgabe 4/Newton.py
+++ Aufgabe 4/Newton.py
@@ -4,18 +4,16 @@ print("Authors: Tobias Stöhr, Lukas Butscher, Wiebke Prinz, Jona Böcker\n")
 
 TOL = 1e-12  # Abbruchbedingung
 f = lambda y: 1 / 3 * m.sqrt((100 - y) ** 2 + 100 ** 2) + 1 / 5 * m.sqrt(100 ** 2 + y ** 2)  # Funktion
-Df = lambda y: ((y - 100) / (3 * m.sqrt((100 - y) ** 2 + 100 ** 2))) + (y / (5 * m.sqrt(100 ** 2 + y ** 2)))  # Abl
-
-
-#f = lambda x: x ** 3 - x ** 2 - 1  # Funktion
-#Df = lambda x: 3 * x ** 2 - 2 * x  # Ableitung
+Df = lambda y: ((y - 100) / (3 * m.sqrt((100 - y) ** 2 + 100 ** 2))) + y / (5 * m.sqrt(100 ** 2 + y ** 2))  # Abl
+# DDf = lambda y: 1 / (5 * m.sqrt(y ** 2 + 10000)) - y ** 2 / (5 * (y ** 2 + 10000) ** 1.5) + (100 - y) * (y - 100) / (3 * ((100 - y) ** 2 + 10000) ** 1.5) + 1 / (3 * m.sqrt((100 - y) ** 2) + 10000)
+DDf = lambda y: 10000 * (y ** 2 + 10000) ** 1.5 + 6000 * ((100 - y) ** 2 + 10000) ** 1.5 / (3*((100 - y) ** 2 + 10000) ** 1.5 * (y ** 2 + 10000) ** 1.5)
 
 
 def newton(x0, max_iter):
     xn = x0
     for n in range(0, max_iter):
-        fxn = f(xn)
-        if abs(fxn) < TOL:
+        Dfxn = Df(xn)
+        if abs(Dfxn) < TOL:
             print('Lösung nach', n, 'durchläufen gefunden.')
             return xn
         Dfxn = Df(xn)
@@ -27,10 +25,9 @@ def newton(x0, max_iter):
     return None
 
 
-# print("", Df(108))
-
-approx = newton(100, 1000)
+x0 = 1
+approx = newton(x0, 10000000)
 if approx is not None:
-    print(round(approx, 6))  # runden des Ergebnisses auf 6 Nachkommastellen
+    print(round(approx, 12))  # runden des Ergebnisses auf 6 Nachkommastellen
 else:
     print(approx)
