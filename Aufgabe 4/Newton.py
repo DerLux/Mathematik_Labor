97d588a17f0719485c40c636c0c91e454e8433dc
DerLux <lukas.butscher@gmx.de>
2022-05-24 14:31:07 +0200

    für Tobi

--- Aufgabe 4/Newton.py
+++ Aufgabe 4/Newton.py
@@ -2,20 +2,20 @@ import math as m
 
 print("Authors: Tobias Stöhr, Lukas Butscher, Wiebke Prinz, Jona Böcker\n")
 
-epsilon = 1e-10  # Abbruchbedingung
-f = lambda y: 1 / 3 * m.sqrt((100 - y) ** 2 + 100 ** 2) + 1 / 4 * m.sqrt(100 ** 2 + y ** 2)  # Funktion
-Df = lambda y: ((y - 100) / 3 * m.sqrt((100 - y) ** 2 + 100 ** 2)) + (y / (5 * m.sqrt(100 ** 2 + y ** 2)))  # Ableitung
+TOL = 1e-12  # Abbruchbedingung
+f = lambda y: 1 / 3 * m.sqrt((100 - y) ** 2 + 100 ** 2) + 1 / 5 * m.sqrt(100 ** 2 + y ** 2)  # Funktion
+Df = lambda y: ((y - 100) / (3 * m.sqrt((100 - y) ** 2 + 100 ** 2))) + (y / (5 * m.sqrt(100 ** 2 + y ** 2)))  # Abl
 
 
-# f = lambda x: x ** 3 - x ** 2 - 1  # Funktion
-# Df = lambda x: 3 * x ** 2 - 2 * x  # Ableitung
+#f = lambda x: x ** 3 - x ** 2 - 1  # Funktion
+#Df = lambda x: 3 * x ** 2 - 2 * x  # Ableitung
 
 
 def newton(x0, max_iter):
     xn = x0
     for n in range(0, max_iter):
         fxn = f(xn)
-        if abs(fxn) < epsilon:
+        if abs(fxn) < TOL:
             print('Lösung nach', n, 'durchläufen gefunden.')
             return xn
         Dfxn = Df(xn)
@@ -27,10 +27,10 @@ def newton(x0, max_iter):
     return None
 
 
-# x = Symbol('x')
-# Dp = p.diff(x) # Ableitung automatisch berechnen
-approx = newton(1, 10)
+# print("", Df(108))
+
+approx = newton(100, 1000)
 if approx is not None:
-    print(round(approx, 6))  # runden des Ergebnisses auf 6 nachkommastellen
+    print(round(approx, 6))  # runden des Ergebnisses auf 6 Nachkommastellen
 else:
     print(approx)
